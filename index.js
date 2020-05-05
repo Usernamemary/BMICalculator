a4160688986993a731d80786488ca77ab06b015b
Usernamemary <47636166+Usernamemary@users.noreply.github.com>
2020-05-04 22:00:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,178 +1,169 @@
-var questions = [
-  {
-    question: "What's your weight in kilograms? (Example: 65)",
-    pattern: /^[+-]?\d+(\.\d+)?$/
-  },
-  {
-    question: "What's your height in meters? (Example: 1.80) ",
-    pattern: /^[+-]?\d+(\.\d+)?$/
-  }
-];
+var questionWeight = document.querySelector(#questionWeight);
+var questionHeight = document.querySelector(#questionHeight);
 
-/**********
-Mary's Note: This is the original codepen I just changed the values and added the formula
 
-  !!!!!
-  New Version: https://codepen.io/arcs/pen/rYXrNQ
-  !!!!!
-  
-  Credits for the design go to XavierCoulombeM
-  https://dribbble.com/shots/2510592-Simple-register-form
-  
-  This Pen uses no libraries except fonts and should 
-  work on all modern browsers
-  
-  The answers are stored in the `questions` array
-  
-  with the key `value`. 
 
- **********/
 
-(function() {
-  var tTime = 100; // transition transform time from #register in ms
-  var wTime = 200; // transition width time from #register in ms
-  var eTime = 1000; // transition width time from inputLabel in ms
+// var questions = [
+//   {
+//     question: "What's your weight in kilograms? (Example: 65)",
+//     pattern: /^[+-]?\d+(\.\d+)?$/,
+//   },
+//   {
+//     question: "What's your height in meters? (Example: 1.80) ",
+//     pattern: /^[+-]?\d+(\.\d+)?$/,
+//   },
+// ];
 
-  // init
-  // --------------
-  var position = 0;
+// /**********
 
-  putQuestion();
+//  **********/
 
-  progressButton.addEventListener('click', validate);
-  inputField.addEventListener('keyup', function(e) {
-    transform(0, 0); // ie hack to redraw
-    if (e.keyCode == 13) validate();
-  });
+// (function () {
+//   var tTime = 100; // transition transform time from #register in ms
+//   var wTime = 200; // transition width time from #register in ms
+//   var eTime = 1000; // transition width time from inputLabel in ms
 
-  // functions
-  // --------------
+//   // init
+//   // --------------
+//   var position = 0;
 
-  // load the next question
-  function putQuestion() {
-    inputLabel.innerHTML = questions[position].question;
-    inputField.value = '';
-    inputField.type = questions[position].type || 'text';
-    inputField.focus();
-    showCurrent();
-  }
+//   putQuestion();
 
-  function bmiCalculator(weight, height) {
-    bmi = (weight / Math.pow(height, 2)).toFixed(2);
-    if (bmi < 18.5) {
-      return (
-        'Your BMI is ' +
-        bmi +
-        ' so you are underweight.' +
-        ' Please visit your doctor.'
-      );
-    } else if (bmi >= 18.5 && bmi <= 24.9) {
-      return (
-        'Your BMI is ' + bmi + ' ¡Congrats! You are on your normal weight.'
-      );
-    } else if (bmi > 24.9 && bmi < 30) {
-      return (
-        'Your BMI is ' +
-        bmi +
-        ' so you are overweight.' +
-        ' Please visit your doctor. Check out some exercises or diets to apply in you everyday routine'
-      );
-    } else if (bmi >= 30 && bmi < 35) {
-      return (
-        'Your BMI is ' +
-        bmi +
-        ' so you are obese.' +
-        ' Please visit your doctor. Remember that there are multiple factors for obesity you might want to know the reason'
-      );
-    } else if (bmi > 34) {
-      return (
-        'Your BMI is ' +
-        bmi +
-        ' so you are extremely obese.' +
-        ' Please visit your doctor as soon as possible. There are multiple factors for obesity is not fun nor healthy, you might want to know the reason'
-      );
-    } else {
-      return 'Please enter your height in meters and height in kilograms';
-    }
-  }
+//   progressButton.addEventListener('click', validate);
+//   inputField.addEventListener('keyup', function (e) {
+//     transform(0, 0); // ie hack to redraw
+//     if (e.keyCode == 13) validate();
+//   });
 
-  // when all the questions have been answered
-  function done() {
-    // remove the box if there is no next question
-    register.className = 'close';
+//   // functions
+//   // --------------
 
-    // add the h1 at the end with the welcome text
+//   // load the next question
+//   function putQuestion() {
+//     inputLabel.innerHTML = questions[position].question;
+//     inputField.value = '';
+//     inputField.type = questions[position].type || 'text';
+//     inputField.focus();
+//     showCurrent();
+//   }
 
-    var h1 = document.createElement('h1');
+//   function bmiCalculator(weight, height) {
+//     bmi = (weight / Math.pow(height, 2)).toFixed(2);
+//     if (bmi < 18.5) {
+//       return (
+//         'Your BMI is ' +
+//         bmi +
+//         ' so you are underweight.' +
+//         ' Please visit your doctor.'
+//       );
+//     } else if (bmi >= 18.5 && bmi <= 24.9) {
+//       return (
+//         'Your BMI is ' + bmi + ' ¡Congrats! You are on your normal weight.'
+//       );
+//     } else if (bmi > 24.9 && bmi < 30) {
+//       return (
+//         'Your BMI is ' +
+//         bmi +
+//         ' so you are overweight.' +
+//         ' Please visit your doctor. Check out some exercises or diets to apply in you everyday routine'
+//       );
+//     } else if (bmi >= 30 && bmi < 35) {
+//       return (
+//         'Your BMI is ' +
+//         bmi +
+//         ' so you are obese.' +
+//         ' Please visit your doctor. Remember that there are multiple factors for obesity you might want to know the reason'
+//       );
+//     } else if (bmi > 34) {
+//       return (
+//         'Your BMI is ' +
+//         bmi +
+//         ' so you are extremely obese.' +
+//         ' Please visit your doctor as soon as possible. There are multiple factors for obesity is not fun nor healthy, you might want to know the reason'
+//       );
+//     } else {
+//       return 'Please enter your height in meters and height in kilograms';
+//     }
+//   }
 
-    h1.appendChild(
-      document.createTextNode(
-        bmiCalculator(questions[0].value, questions[1].value)
-      )
-    );
-    setTimeout(function() {
-      register.parentElement.appendChild(h1);
-      setTimeout(function() {
-        h1.style.opacity = 1;
-      }, 50);
-    }, eTime);
-  }
+//   // when all the questions have been answered
+//   function done() {
+//     // remove the box if there is no next question
+//     register.className = 'close';
 
-  // when submitting the current question
-  function validate() {
-    // set the value of the field into the array
-    questions[position].value = inputField.value;
+//     // add the h1 at the end with the welcome text
 
-    // check if the pattern matches
-    if (!inputField.value.match(questions[position].pattern || /.+/)) wrong();
-    else
-      ok(function() {
-        // set the progress of the background
-        progress.style.width = (++position * 100) / questions.length + 'vw';
+//     var h1 = document.createElement('h1');
 
-        // if there is a new question, hide current and load next
-        if (questions[position]) hideCurrent(putQuestion);
-        else hideCurrent(done);
-      });
-  }
+//     h1.appendChild(
+//       document.createTextNode(
+//         bmiCalculator(questions[0].value, questions[1].value)
+//       )
+//     );
+//     setTimeout(function () {
+//       register.parentElement.appendChild(h1);
+//       setTimeout(function () {
+//         h1.style.opacity = 1;
+//       }, 50);
+//     }, eTime);
+//   }
 
-  // helper
-  // --------------
+//   // when submitting the current question
+//   function validate() {
+//     // set the value of the field into the array
+//     questions[position].value = inputField.value;
 
-  function hideCurrent(callback) {
-    inputContainer.style.opacity = 0;
-    inputProgress.style.transition = 'none';
-    inputProgress.style.width = 0;
-    setTimeout(callback, wTime);
-  }
+//     // check if the pattern matches
+//     if (!inputField.value.match(questions[position].pattern || /.+/)) wrong();
+//     else
+//       ok(function () {
+//         // set the progress of the background
+//         progress.style.width = (++position * 100) / questions.length + 'vw';
 
-  function showCurrent(callback) {
-    inputContainer.style.opacity = 1;
-    inputProgress.style.transition = '';
-    inputProgress.style.width = '100%';
-    setTimeout(callback, wTime);
-  }
+//         // if there is a new question, hide current and load next
+//         if (questions[position]) hideCurrent(putQuestion);
+//         else hideCurrent(done);
+//       });
+//   }
 
-  function transform(x, y) {
-    register.style.transform = 'translate(' + x + 'px ,  ' + y + 'px)';
-  }
+//   // helper
+//   // --------------
 
-  function ok(callback) {
-    register.className = '';
-    setTimeout(transform, tTime * 0, 0, 10);
-    setTimeout(transform, tTime * 1, 0, 0);
-    setTimeout(callback, tTime * 2);
-  }
+//   function hideCurrent(callback) {
+//     inputContainer.style.opacity = 0;
+//     inputProgress.style.transition = 'none';
+//     inputProgress.style.width = 0;
+//     setTimeout(callback, wTime);
+//   }
 
-  function wrong(callback) {
-    register.className = 'wrong';
-    for (
-      var i = 0;
-      i < 6;
-      i++ // shaking motion
-    )
-      setTimeout(transform, tTime * i, ((i % 2) * 2 - 1) * 20, 0);
-    setTimeout(transform, tTime * 6, 0, 0);
-    setTimeout(callback, tTime * 7);
-  }
-})();
+//   function showCurrent(callback) {
+//     inputContainer.style.opacity = 1;
+//     inputProgress.style.transition = '';
+//     inputProgress.style.width = '100%';
+//     setTimeout(callback, wTime);
+//   }
+
+//   function transform(x, y) {
+//     register.style.transform = 'translate(' + x + 'px ,  ' + y + 'px)';
+//   }
+
+//   function ok(callback) {
+//     register.className = '';
+//     setTimeout(transform, tTime * 0, 0, 10);
+//     setTimeout(transform, tTime * 1, 0, 0);
+//     setTimeout(callback, tTime * 2);
+//   }
+
+//   function wrong(callback) {
+//     register.className = 'wrong';
+//     for (
+//       var i = 0;
+//       i < 6;
+//       i++ // shaking motion
+//     )
+//       setTimeout(transform, tTime * i, ((i % 2) * 2 - 1) * 20, 0);
+//     setTimeout(transform, tTime * 6, 0, 0);
+//     setTimeout(callback, tTime * 7);
+//   }
+// })();
